c67dd5d49513d7472423625f9c1686ecc68e546f
Étienne Reuh Fildadut <fildadut@reuh.eu>
2025-06-25 13:15:41 +0200

    Initial setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y borgbackup borgmatic btrfsmaintenance fish glances micro plocate screen smartmontools snapper 
 
 # Use a COPR Example:
 #
@@ -22,3 +22,9 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable podman-restart.service
+systemctl enable podman-auto-update.timer
+systemctl enable btrfsmaintenance-refresh.path
+systemctl enable plocate-updatedb.timer
+systemctl enable snapper-timeline.timer
+systemctl enable snapper-cleanup.timer
